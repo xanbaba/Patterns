73ff744d506a0ea675bb7e660f96bfe4acaca869
Fatul_gb62 <fatul_gb62@itstep.edu.az>
2024-05-19 21:49:38 +0400

    Xanatom Start HW

---
+++ Xanatom/Handler/BaseHandler.cs
@@ -0,0 +1,33 @@
+﻿using Xanatom.Model;
+
+namespace Xanatom.Handler;
+
+public abstract class BaseHandler<T> : IHandler<T> where T : class
+{
+    public IHandler<T>? Next { get; set; }
+    public abstract void Handle(T data);
+}
+
+public class BankCardNumberAuthenticationHandler : BaseHandler<XanatomHandlerData>
+{
+    public override void Handle(XanatomHandlerData data)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class BankCardOwnerNameAuthenticationHandler : BaseHandler<XanatomHandlerData>
+{
+    public override void Handle(XanatomHandlerData data)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class BankCardExpireDateAuthenticationHandler : BaseHandler<XanatomHandlerData>
+{
+    public override void Handle(XanatomHandlerData data)
+    {
+        throw new NotImplementedException();
+    }
+}
